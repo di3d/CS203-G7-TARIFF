d32619fc786d60fd1c2a735bbdbfc23317f7e72a
yanhui <noth.tanyh@gmail.com>
2025-11-10 19:57:31 +0800

    removed pointless console.log

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_TARIFFIC_BACKEND_URL;
 
-console.log(process.env.NEXT_PUBLIC_TARRIFIC_BACKEND_URL);
-
 export const api = axios.create({
     baseURL: API_BASE_URL,
 });
